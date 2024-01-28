61a0754cc443f9c827c2ff23b13db9059d78b72d
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2024-01-28 17:21:02 +0100

    Add animations

---
+++ animation/templates/src/project.ts
@@ -0,0 +1,9 @@
+import {makeProject} from '@motion-canvas/core';
+
+import why from './scenes/why?scene';
+
+import './global.css'; // <- import the css
+
+export default makeProject({
+  scenes: [why],
+});
