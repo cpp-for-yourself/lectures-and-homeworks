a484a42b380267f732b2b42018aef523caf74ad2
Igor Bogoslavskyi <niosus@users.noreply.github.com>
2026-05-19 21:59:32 +0200

    Add centerOn function to the new animation process (#122)
    Add centerOn function to animation projects

--- animation/projects/src/dummy/scenes/dummy.tsx
+++ animation/projects/src/dummy/scenes/dummy.tsx
@@ -3,6 +3,9 @@ import { makeScene2D, Code, LezerHighlighter } from '@motion-canvas/2d';
 import { all, createRef, waitFor } from '@motion-canvas/core';
 import { MyStyle } from '../../styles';
 import { parser as parser_cpp } from '@lezer/cpp';
+import { DEFAULT } from '@motion-canvas/core/lib/signals';
+import { centerOn } from '../../utils';
+import { lines } from '@motion-canvas/2d';
 
 import dummyCode from '@lectures/dummy.md?snippet=dummy_snippet/main.cpp';
 
@@ -17,11 +20,13 @@ export default makeScene2D(function* (view) {
             fontSize={28}
             fontFamily={'Fira Mono'}
             highlighter={CppHighlighter}
-            code={''}
+            code={dummyCode}
         />
     );
 
-    yield* waitFor(0.5);
-    yield* codeRef().code(dummyCode, 1);
+    yield* centerOn(codeRef(), DEFAULT, 1, 36);
     yield* waitFor(1);
+
+    yield* centerOn(codeRef(), lines(3, 3), 1, 40);
+    yield* waitFor(3);
 });
